7417a99ae95435c311585252670c252086f253f2
MarceloDev-XY <marcelobaraujo@yahoo.com.br>
2020-03-15 14:42:26 -0300

    inclusão de informações nas tables

--- 120220_AvBancoDados.sql
+++ 120220_AvBancoDados.sql
@@ -63,14 +63,57 @@ go
 	(115, 'Tatuapé' , 'Rua Tuiti, 50' , 1),
 	(120, 'Centro SP' , 'Praça da Sé' , 1)
 go
-/*	Inclusão de dados table */
+/*	Inclusão de dados table conta*/
 
 	insert into tpconta	values
 	(10, 'Conta Corrente'),
 	(11, 'Conta Poupança'),
 	(12, 'Conta Investimento')
 go
+/* Inclusão de dados table cliente */
+
+	insert into cliente values
+	(111, 'Jose Silva', '1980-01-15'),
+	(112, 'Marcia Silva Martins', '1990-05-08'),
+	(113, 'Mario Andarins', '1991-02-28'),
+	(114, 'Giovana Silva Teles', '1992-01-02')
+go
+/* Inclusão de dados table conta */
+
+	insert into conta values
+	(1, 10,	 114,  110,	'2500.10'),
+	(2, 10,  111,  115,	'0.15'),
+	(3, 12,  111,  115,	'1800.00'),
+	(4, 11,  114,  120,	'300.20'),
+	(5, 12,	 111,  110,	'500.00'),
+	(6, 11,	 113,  110,	'1250.10')
+go
+
+select * from banco
+select * from tpconta
+select * from cliente
+select * from agencia
+select * from conta
+
+/* Incluindo email cliente */
+alter table cliente
+	add		email_clie		varchar (60)		null;
+go
+
+/* Inclusão de dados email para cliente*/
+	update	cliente
+	set		email_clie = 'Mario_Andarins@gmail.com'
+	where	id_clie = 113;
+go
+
+/* Selecionando as table */
+
+select * from tpconta
+
 
 
 	
+
+
+
 
